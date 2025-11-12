fb8de9126ff92733cda149b61689c4aea686829b
jonortega20 <jonortega20@gmail.com>
2025-11-12 16:01:43 +0100

    layer normalization implemented

--- multihead-attention.py
+++ multihead-attention.py
@@ -122,10 +122,12 @@ class Block(nn.Module):
         head_size = n_embd // n_head
         self.sa = MultiHeadAttention(n_head, head_size)
         self.ffwd = FeedForward(n_embd)
+        self.layernorm1 = nn.LayerNorm(n_embd)
+        self.layernorm2 = nn.LayerNorm(n_embd)
 
     def forward(self, x):
-        x = x + self.sa(x)   # the sum is due to residual connections
-        x = x + self.ffwd(x) # the sum is due to residual connections
+        x = x + self.sa(self.layernorm1(x))   # the sum is due to residual connections
+        x = x + self.ffwd(self.layernorm2(x)) # the sum is due to residual connections
         return x
 
 
